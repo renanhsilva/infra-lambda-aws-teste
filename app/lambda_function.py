d3802a787bca5a26cfe7c0d992124412b7260d0b
Renan Silva <renanh2008@hotmail.com>
2023-11-03 04:11:14 -0300

    teste token

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -6,3 +6,23 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('Teste')
     }
+    import json
+    from validate_docbr import CPF
+
+    def lambda_handler(event, context):
+        # Recebe o CPF do usuário
+        cpf = event['cpf']
+
+        # Verifica se o CPF é válido
+        if CPF().validate(cpf):
+            # CPF válido, retorna sucesso
+            return {
+                'statusCode': 200,
+                'body': json.dumps('Usuário autenticado com sucesso!')
+            }
+        else:
+            # CPF inválido, retorna erro
+            return {
+                'statusCode': 400,
+                'body': json.dumps('CPF inválido!')
+            }
