a223f4a02ccaf0e722811f01394b7e60f17896a3
Renan Silva <renanh2008@hotmail.com>
2023-11-03 04:11:37 -0300

    update

--- app/lambda_function.py
+++ app/lambda_function.py
@@ -1,28 +1,57 @@
 import json
+import boto3
+import jwt
+
+SECRET_NAME = "seu-secret-name"  # Nome do segredo no AWS Secret Manager
 
 def lambda_handler(event, context):
-    # TODO implement
+    # Valide o CPF a partir do evento de entrada (event)
+    cpf = event.get("cpf")
+    if not validate_cpf(cpf):
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "CPF inválido"})
+        }
+
+    # Recupere o segredo do AWS Secret Manager
+    secret_value = get_secret_value(SECRET_NAME)
+
+    if not secret_value:
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": "Falha ao recuperar o segredo"})
+        }
+
+    secret = secret_value["SecretString"]
+
+    # Gere um token JWT usando o segredo
+    jwt_token = generate_jwt_token(cpf, secret)
+
     return {
-        'statusCode': 200,
-        'body': json.dumps('Teste')
+        "statusCode": 200,
+        "body": json.dumps({"token": jwt_token})
     }
-    import json
-    from validate_docbr import CPF
 
-    def lambda_handler(event, context):
-        # Recebe o CPF do usuário
-        cpf = event['cpf']
+def validate_cpf(cpf):
+    # Implemente a validação do CPF aqui
+    # Retorne True se o CPF for válido e False se for inválido
+    if cpf == "12345678900":
+        return True
+    pass
 
-        # Verifica se o CPF é válido
-        if CPF().validate(cpf):
-            # CPF válido, retorna sucesso
-            return {
-                'statusCode': 200,
-                'body': json.dumps('Usuário autenticado com sucesso!')
-            }
-        else:
-            # CPF inválido, retorna erro
-            return {
-                'statusCode': 400,
-                'body': json.dumps('CPF inválido!')
-            }
+def get_secret_value(secret_name):
+    # Conecte-se ao AWS Secret Manager e recupere o valor do segredo
+    client = boto3.client("secretsmanager")
+    response = client.get_secret_value(SecretId=secret_name)
+
+    if "SecretString" in response:
+        return json.loads(response["SecretString"])
+    else:
+        return None
+
+def generate_jwt_token(cpf, secret):
+    # Gere um token JWT usando a biblioteca PyJWT
+    token_payload = {"cpf": cpf}
+    jwt_token = jwt.encode(token_payload, secret, algorithm="HS256")
+
+    return jwt_token
